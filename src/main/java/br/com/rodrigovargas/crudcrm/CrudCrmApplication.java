ba3bb4a2221ffd3ad5b3b30e92fd1b807ca35230
Alessandro Lima <alessandroslima@hotmail.com>
2022-08-02 18:08:54 -0300

    Setup do projeto

---
+++ src/main/java/br/com/rodrigovargas/crudcrm/CrudCrmApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigovargas.crudcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodrigovargas/crudcrm/CrudCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigovargas.crudcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
